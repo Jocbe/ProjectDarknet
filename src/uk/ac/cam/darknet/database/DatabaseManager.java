4068109e481fc23dfbc8cdb0e5473ce5655de46d
Augustin Zidek <augustin@zidek.eu>
2014-02-01 16:20:19 +0000

    My first commit: Added class DatabaseManager

---
+++ src/uk/ac/cam/darknet/database/DatabaseManager.java
@@ -0,0 +1,12 @@
+package uk.ac.cam.darknet.database;
+
+/**
+ * @author Augustin Zidek
+ * 
+ */
+public class DatabaseManager {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world, this is my first commit.");
+	}
+}

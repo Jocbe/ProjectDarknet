27cb6eea069b753a139fd21a57cbdcbd75874e13
Jocbe <jocbe@atbel.de>
2014-02-09 12:44:45 +0000

    Updated logger formatting

--- src/uk/ac/cam/darknet/common/LoggerFactory.java
+++ src/uk/ac/cam/darknet/common/LoggerFactory.java
@@ -1,10 +1,16 @@
 package uk.ac.cam.darknet.common;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 /**
  * Factory for a logger to ensure there is only one logger in the system.
@@ -21,12 +27,63 @@ public class LoggerFactory {
 			logger.setLevel(Level.ALL);
 
 			final FileHandler fh;
+			final ConsoleHandler ch;
 
 			try {
+				
 				fh = new FileHandler(Strings.LOG_PATH, true);
-				fh.setFormatter(new SimpleFormatter());
+				ch = new ConsoleHandler();
+				
+				
+				class LogFormatter extends Formatter {
+					
+					@Override
+					public String format(LogRecord r) {
+
+						StringBuilder sb = new StringBuilder();
+						
+						String d = (new SimpleDateFormat("yyyy-mm-dd_hh:mm:ss")).format(new Date(r.getMillis()));
+						
+						sb.append(d)
+							.append(" [")
+							.append(r.getLevel().getLocalizedName())
+							.append("] - ")
+							.append(r.getSourceMethodName())
+							.append(" (")
+							.append(r.getSourceClassName())
+							.append("): ")
+							.append(formatMessage(r))
+							.append("\n");
+						
+						if(r.getThrown() != null) {
+							try{
+								StringWriter sw = new StringWriter();
+								try(PrintWriter pw = new PrintWriter(sw)) {
+									r.getThrown().printStackTrace(pw);
+								}
+								sb.append(sw.toString());
+							} catch(Exception e) {
+								System.err.println("Exception occurred in conditional block of log formatter "
+										+ "while trying to format a log entry! Check. Exception type: "
+										+ e.getClass().toString() + ". Message: " + e.getMessage());
+							}
+						}
+						
+						return sb.toString();
+						
+					}
+				}
+				
+				LogFormatter lf = new LogFormatter();
+				
+				fh.setFormatter(lf);
+				ch.setFormatter(lf);
+
+				logger.setUseParentHandlers(false);
 
 				logger.addHandler(fh);
+				logger.addHandler(ch);
+				
 			}
 			catch (IOException e) {
 				System.err.println(Strings.LOG_EXCEPTION + e.getMessage());

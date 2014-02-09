7421df00e1f6605bbac41da982b51e01a15574b1
Ibtehaj Nadeem <ian25@cam.ac.uk>
2014-02-09 17:47:27 +0000

    Added the SQLTool

--- src/uk/ac/cam/darknet/backend/DataCollectionManager.java
+++ src/uk/ac/cam/darknet/backend/DataCollectionManager.java
@@ -1,8 +1,9 @@
 package uk.ac.cam.darknet.backend;
 
 /**
- * Handles and coordinates the data collection. Calls the primary data
- * collectors and then selected secondary data collectors.
+ * Handles and coordinates the data collection. Responsible for creating the
+ * global attribute table and appropriate database managers. Calls the primary
+ * data collectors and then selected secondary data collectors.
  * 
  * @author Augustin Zidek
  */

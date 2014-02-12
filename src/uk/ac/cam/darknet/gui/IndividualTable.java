d1ae0d62697252a1f00ba23c5c36df6f0a6ce0bf
Farah Patel <fpatel@mit.edu>
2014-02-12 22:26:57 +0000

    Twitter documentation seems to have unconnected from repository. Adding back in.

---
+++ src/uk/ac/cam/darknet/gui/IndividualTable.java
@@ -0,0 +1,63 @@
+package uk.ac.cam.darknet.gui;
+
+import java.util.List;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import uk.ac.cam.darknet.common.Individual;
+
+/**
+ * A Swing table extending JTable to store individuals. Provides methods such as
+ * addIndividual(), which add the given individual to the table and display it.
+ * 
+ * @author Augustin Zidek
+ * 
+ */
+public class IndividualTable extends JTable {
+	private static final long serialVersionUID = 1L;
+	private static final String[] columns = { "ID", "First name", "Last name",
+			"Email", "Event date", "Seat" };
+	final static DefaultTableModel model = new DefaultTableModel();
+
+	/**
+	 * Creates a new empty table with the column names set to the primary fields
+	 * of the Individual, i.e. id, firstName, lastName, email, eventDate, seat.
+	 * 
+	 */
+	public IndividualTable() {
+		super(model);
+		// Set the columns
+		model.setColumnIdentifiers(columns);
+		// Create auto sorted for each column
+		this.setAutoCreateRowSorter(true);
+		// Set the column widths
+		this.getColumnModel().getColumn(0).setPreferredWidth(50);
+		this.getColumnModel().getColumn(4).setPreferredWidth(60);
+		this.getColumnModel().getColumn(5).setPreferredWidth(25);
+	}
+
+	/**
+	 * Displays the primary data of the given individuals in the table, one
+	 * individual per row.
+	 * 
+	 * @param individuals The list of individuals.
+	 */
+	public void displayIndividuals(final List<Individual> individuals) {
+		final DefaultTableModel model = (DefaultTableModel) this.getModel();
+
+		// Go through all individuals, get their fields and display
+		for (final Individual i : individuals) {
+			final long ID = i.getId();
+			final String firstName = i.getFirstName();
+			final String lastName = i.getLastName();
+			final String email = i.getEmail();
+			final String eventDate = i.getEventDate().toString();
+			final String seat = i.getSeat();
+
+			model.addRow(new Object[] { ID, firstName, lastName, email,
+					eventDate, seat });
+		}
+	}
+
+}

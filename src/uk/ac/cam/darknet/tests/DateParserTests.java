545306650a2d65e9f074bbf4d71d99b79a8e111b
Augustin Zidek <augustin@zidek.eu>
2014-02-19 15:40:54 +0000

    TODO file update

---
+++ src/uk/ac/cam/darknet/tests/DateParserTests.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.darknet.tests;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParserTests {
+	public static void main(String[] args) throws ParseException {
+		final Date eventDate = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss")
+				.parse("03/07/2011 19:30:00");
+
+		System.out.println(eventDate.toString());
+	}
+
+}

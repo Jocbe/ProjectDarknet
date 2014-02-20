4911ae512fec7702abd9da9f2c97657306cbf8b3
Farah Patel <fpatel@mit.edu>
2014-02-20 11:32:46 +0000

    updated effect so it takes in Individuals for execution

--- src/uk/ac/cam/darknet/frontend/Effect.java
+++ src/uk/ac/cam/darknet/frontend/Effect.java
@@ -1,8 +1,12 @@
 package uk.ac.cam.darknet.frontend;
 
+import java.util.List;
+
+import uk.ac.cam.darknet.common.Individual;
+
 /**
- * An interface for effects. An effect is something that queries and makes use of the data in the
- * system.
+ * An interface for effects. An effect is something that queries and makes use
+ * of the data in the system.
  * 
  * @author Augustin Zidek
  * 
@@ -10,9 +14,13 @@ package uk.ac.cam.darknet.frontend;
 public interface Effect {
 
 	/**
-	 * Executes the given effect, which involves fetching data, processing it and displaying it in
-	 * some format.
+	 * Executes the given effect on the specified individuals, which involves
+	 * fetching data, processing it and displaying it in some format.
+	 * 
+	 * @param individuals
+	 *            List of individuals the effect should be performed upon,
+	 *            provided EffectGUI.
 	 */
-	public void execute();
+	public void execute(List<Individual> individuals);
 
 }
--- src/uk/ac/cam/darknet/frontend/PictureWallEffect.java
+++ src/uk/ac/cam/darknet/frontend/PictureWallEffect.java
@@ -1,5 +1,9 @@
 package uk.ac.cam.darknet.frontend;
 
+import java.util.List;
+
+import uk.ac.cam.darknet.common.Individual;
+
 /**
  * An effect for generating a wall of pictures of all the people in the theatre (i.e. all
  * individuals in the database for the given performance).
@@ -9,7 +13,7 @@ package uk.ac.cam.darknet.frontend;
  */
 public class PictureWallEffect implements Effect {
 	@Override
-	public void execute() {
+	public void execute(List<Individual> individuals) {
 		// TODO Auto-generated method stub
 	}
 }

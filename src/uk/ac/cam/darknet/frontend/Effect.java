ed93cda6dd6101b3d85dc5a9beda2464eba73f73
Ibtehaj Nadeem <ian25@cam.ac.uk>
2014-02-23 19:36:39 +0000

    Changed the Effects.execute() method to take a Show object as a parameter rather than a list of individuals. This is more sensible because the effect is unlikely to use all the individuals in a particular show and would need to filter them anyway. Being given a show it is possible to get suitable individuals that attend a particular show.

--- src/uk/ac/cam/darknet/frontend/Effect.java
+++ src/uk/ac/cam/darknet/frontend/Effect.java
@@ -1,12 +1,10 @@
 package uk.ac.cam.darknet.frontend;
 
-import java.util.List;
-
-import uk.ac.cam.darknet.common.Individual;
+import uk.ac.cam.darknet.common.Show;
 
 /**
- * An interface for effects. An effect is something that queries and makes use
- * of the data in the system.
+ * An interface for effects. An effect is something that queries and makes use of the data in the
+ * system.
  * 
  * @author Augustin Zidek
  * 
@@ -14,13 +12,12 @@ import uk.ac.cam.darknet.common.Individual;
 public interface Effect {
 
 	/**
-	 * Executes the given effect on the specified individuals, which involves
-	 * fetching data, processing it and displaying it in some format.
+	 * Executes the given effect on the specified individuals, which involves fetching data,
+	 * processing it and displaying it in some format.
 	 * 
 	 * @param individuals
-	 *            List of individuals the effect should be performed upon,
-	 *            provided EffectGUI.
+	 *            List of individuals the effect should be performed upon, provided EffectGUI.
 	 */
-	public void execute(List<Individual> individuals);
+	public void execute(Show show);
 
 }

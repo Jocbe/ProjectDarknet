8bc6e326eb9da9e8b03f8d646bbd6a92c5c9c871
Farah Patel <fpatel@mit.edu>
2014-02-25 11:57:29 +0000

    fixes to PictureWallEffect and effect

--- src/uk/ac/cam/darknet/frontend/PictureWallEffect.java
+++ src/uk/ac/cam/darknet/frontend/PictureWallEffect.java
@@ -3,17 +3,26 @@ package uk.ac.cam.darknet.frontend;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
 
 import uk.ac.cam.darknet.common.AttributeCategories;
 import uk.ac.cam.darknet.common.Individual;
+import uk.ac.cam.darknet.common.IndividualRequirements;
 import uk.ac.cam.darknet.common.Properties;
 import uk.ac.cam.darknet.common.Show;
 import uk.ac.cam.darknet.database.DatabaseManager;
 import uk.ac.cam.darknet.database.SecondaryDatabaseManager;
+import uk.ac.cam.darknet.exceptions.ConfigFileNotFoundException;
+import uk.ac.cam.darknet.exceptions.InvalidAttributeNameException;
+import uk.ac.cam.darknet.exceptions.InvalidReliabilityException;
+import uk.ac.cam.darknet.exceptions.RequestNotSatisfiableException;
 import uk.ac.cam.darknet.storage.ImageStorage;
 
 /**
@@ -23,69 +32,80 @@ import uk.ac.cam.darknet.storage.ImageStorage;
  * @author Augustin Zidek
  * 
  */
-public class PictureWallEffect implements Effect {
+public class PictureWallEffect extends Effect {
+	
+	//Setup method, DatabaseManager
+	public PictureWallEffect(DatabaseManager dm){
+		super(dm);
+	}
 
 	@Override
 	public void execute(Show show) {
-		// DO I MAKE THIS? OR SHOULD IT BE PASSED? this goes for
-		// globalAttributeTable and DatabaseManager
-		Hashtable<String, AttributeCategories> globalAttributeTable = new Hashtable<String, AttributeCategories>();
-		DatabaseManager manager = new SecondaryDatabaseManager(
-				globalAttributeTable);
-		// get rid of exceptions? if throw would need to change effect interface
-		List<Individual> individuals = manager.getByShow(show);
-		// path would need to be correct one? value should be passed?
-		ImageStorage imgStore = new ImageStorage();
+		IndividualRequirements req = new IndividualRequirements(show);
+		try {
+			req.addRequirement(AttributeCategories.PHOTOS, .75);
+			List<Individual> individuals = this.dm.getSuitableIndividuals(req);
+			ImageStorage imgStore = new ImageStorage();
 
-		// DO I create this here or should it be passed?? or is it not necessary
-		// b/c gotten individuals
-		DataProvider provider = new DataProvider();
-		// should the second parameter be an attribute categories instead of a
-		// String?? and is PHOTO correct?
-		provider.getSpecificAttribute(individuals, "PHOTO");
-
-		BufferedImage[] photos = new BufferedImage[individuals.size()];
-		for (int i = 0; i < photos.length; i++) {
-			Properties p = individuals.get(i).getProperties();
-			photos[i] = (BufferedImage) imgStore.retreiveImage((String) p.get(
-					"photo").getAttribute());
-		}
-
-		BufferedImage image;
-		if (photos.length > 5) {
-			// assemble into columns of 5
-			int numcols = individuals.size() / 5;
-			int[] colwidth = new int[numcols];
-			BufferedImage[] colPhotos = new BufferedImage[numcols];
-			for (int i = 0; i < numcols; i++) {
-				int heightTotal = 0;
-				for (int j = 0; j < 5; j++) {
-					heightTotal += photos[i + j].getHeight();
-					int width = photos[i + j].getWidth();
-					if (width > colwidth[i]) {
-						colwidth[i] = width;
-					}
-				}
-				int heightCurr = 0;
-				BufferedImage colImage = new BufferedImage(colwidth[i],
-						heightTotal, BufferedImage.TYPE_INT_RGB);
-				Graphics2D g2d = colImage.createGraphics();
-				for (int j = 0; j < 5; j++) {
-					g2d.drawImage(photos[i + j], 0, heightCurr, null);
-					heightCurr += photos[i + j].getHeight();
-				}
-				colPhotos[i] = colImage;
-				g2d.dispose();
+			List<String> photoids = new LinkedList<String>();
+			for (int i=0; i<individuals.size(); i++){
+				 photoids.addAll((LinkedList)individuals.get(i).getProperties().get("fb_photos").getAttribute());
+			}
+			
+			BufferedImage[] photos = new BufferedImage[photoids.size()];
+			for (int i = 0; i < photos.length; i++) {
+				photos[i] = (BufferedImage) imgStore.retreiveImage(photoids.get(i));
 			}
 
-			// assemble columns into 1 image
-			image = concatenateByWidth(colPhotos);
-		} else {
-			image = concatenateByWidth(photos);
+			BufferedImage image;
+			if (photos.length > 5) {
+				// assemble into columns of 5
+				int numcols = individuals.size() / 5;
+				int[] colwidth = new int[numcols];
+				BufferedImage[] colPhotos = new BufferedImage[numcols];
+				for (int i = 0; i < numcols; i++) {
+					int heightTotal = 0;
+					for (int j = 0; j < 5; j++) {
+						heightTotal += photos[i + j].getHeight();
+						int width = photos[i + j].getWidth();
+						if (width > colwidth[i]) {
+							colwidth[i] = width;
+						}
+					}
+					int heightCurr = 0;
+					BufferedImage colImage = new BufferedImage(colwidth[i],
+							heightTotal, BufferedImage.TYPE_INT_RGB);
+					Graphics2D g2d = colImage.createGraphics();
+					for (int j = 0; j < 5; j++) {
+						g2d.drawImage(photos[i + j], 0, heightCurr, null);
+						heightCurr += photos[i + j].getHeight();
+					}
+					colPhotos[i] = colImage;
+					g2d.dispose();
+				}
+
+				// assemble columns into 1 image
+				image = concatenateByWidth(colPhotos);
+			} else {
+				image = concatenateByWidth(photos);
+			}
+
+			//ummm pathname?? need input?
+			ImageIO.write(image, "png", new File("pictureEffect.png"));
+		} catch (InvalidReliabilityException e) {
+			//TODO 
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RequestNotSatisfiableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
-		//ummm pathname?? need input?
-		ImageIO.write(image, "png", new File("pictureEffect.png"));
 
 	}
 	
@@ -110,4 +130,13 @@ public class PictureWallEffect implements Effect {
 		g2d.dispose();
 		return image;
 	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, ConfigFileNotFoundException, IOException, SQLException, InvalidAttributeNameException{
+		Hashtable<String,AttributeCategories> globalAttributeTable = new Hashtable<String,AttributeCategories>();
+		DatabaseManager dm = new SecondaryDatabaseManager(globalAttributeTable);
+		PictureWallEffect pwe = new PictureWallEffect(dm);
+		Show show = dm.getAllShows().get(2);
+		pwe.execute(show);
+	}
+	
 }

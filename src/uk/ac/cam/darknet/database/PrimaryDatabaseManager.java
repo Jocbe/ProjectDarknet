c253227f4485a692d5c1db39c4c9df031c9027e6
Ibtehaj Nadeem <ian25@cam.ac.uk>
2014-02-26 15:32:24 +0000

    Test cases, code clean up.

--- src/uk/ac/cam/darknet/database/PrimaryDatabaseManager.java
+++ src/uk/ac/cam/darknet/database/PrimaryDatabaseManager.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -321,21 +320,5 @@ public class PrimaryDatabaseManager extends DatabaseManager {
 		}
 		date = dateToSQLTimestamp(toStore.getEventDate());
 		venue = toStore.getEvenVenue();
-	}
-
-	@SuppressWarnings({"javadoc", "deprecation", "unused"})
-	public static void main(String args[]) throws ClassNotFoundException, ConfigFileNotFoundException, IOException, SQLException {
-		PrimaryDatabaseManager instance = new PrimaryDatabaseManager(null);
-		java.util.Date date = new java.util.Date(2014, 0, 0);
-		ArrayList<Individual> individuals = new ArrayList<Individual>();
-		String[] fnames = {"Claire", "Denise", "Richard", "Travis", "Sheila"};
-		String[] lnames = {"Manzella", "Salazar", "Connally", "Briggs", "Brewer"};
-		String[] emails = {"c.manzella241@gmail.com", "", "", "", "sheilambrewer@teleworm.us"};
-		String[] seats = {"A01", "B52", null, "C04", "D14"};
-		for (int i = 0; i < 128; i++) {
-			individuals.add(Individual.getNewIndividual(fnames[i % 5], lnames[i % 5], emails[i % 5], date, 1, Integer.toString(i), null));
-		}
-		instance.storeIndividual(individuals);
-		instance.closeConnection();
 	}
 }
--- src/uk/ac/cam/darknet/tests/DatabaseManagerTests.java
+++ src/uk/ac/cam/darknet/tests/DatabaseManagerTests.java
@@ -11,6 +11,7 @@ import uk.ac.cam.darknet.common.Individual;
 import uk.ac.cam.darknet.common.IndividualRequirements;
 import uk.ac.cam.darknet.common.Show;
 import uk.ac.cam.darknet.database.DatabaseManager;
+import uk.ac.cam.darknet.database.PrimaryDatabaseManager;
 import uk.ac.cam.darknet.exceptions.ConfigFileNotFoundException;
 import uk.ac.cam.darknet.exceptions.InvalidAttributeTypeException;
 import uk.ac.cam.darknet.exceptions.InvalidReliabilityException;
@@ -42,8 +43,26 @@ public class DatabaseManagerTests {
 			}
 			gender = (String) i.getAttribute("fb_gender").get(0).getAttribute();
 			dob = (String) i.getAttribute("fb_birthday").get(0).getAttribute();
-			System.out.println("Individual " + i.getId() + ", called " + i.getFirstName() + " " + i.getLastName() + ", is " + gender + " and has birthday " + dob + ".");
+			System.out.println(" Individual " + i.getId() + ", called " + i.getFirstName() + " " + i.getLastName() + ", is " + gender + " and has birthday " + dob + ".");
 		}
+		instance.closeConnection();
+	}
+
+	@Test
+	public void TestPrimaryDBM() throws ClassNotFoundException, ConfigFileNotFoundException, IOException, SQLException {
+		Hashtable<String, AttributeCategories> globalAttributeTable = new Hashtable<String, AttributeCategories>();
+		globalAttributeTable.put("fb_gender", AttributeCategories.GENDER);
+		globalAttributeTable.put("fb_birthday", AttributeCategories.BIRTHDAY);
+		PrimaryDatabaseManager instance = new PrimaryDatabaseManager(globalAttributeTable);
+		java.util.Date date = new java.util.Date(2014, 0, 0);
+		ArrayList<Individual> individuals = new ArrayList<Individual>();
+		String[] fnames = {"Claire", "Denise", "Richard", "Travis", "Sheila"};
+		String[] lnames = {"Manzella", "Salazar", "Connally", "Briggs", "Brewer"};
+		String[] emails = {"c.manzella241@gmail.com", "", "", "", "sheilambrewer@teleworm.us"};
+		for (int i = 5000; i < 5010; i++) {
+			individuals.add(Individual.getNewIndividual(fnames[i % 5], lnames[i % 5], emails[i % 5], date, 1, Integer.toString(i), null));
+		}
+		instance.storeIndividual(individuals);
 		instance.closeConnection();
 	}
 

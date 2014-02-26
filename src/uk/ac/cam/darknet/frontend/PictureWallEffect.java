9e3c81aefbd44e72fede0d282a0f30bfc13e5642
Farah Patel <fpatel@mit.edu>
2014-02-26 14:11:35 +0000

    PictureWall effects fixes

--- src/uk/ac/cam/darknet/frontend/PictureWallEffect.java
+++ src/uk/ac/cam/darknet/frontend/PictureWallEffect.java
@@ -36,18 +36,18 @@ import uk.ac.cam.darknet.storage.ImageStorage;
  * 
  */
 public class PictureWallEffect extends Effect {
-	
+
 	private String pathname;
-	
-	//Setup method, DatabaseManager
+
+	// Setup method, DatabaseManager
 	/**
 	 * @param dm
 	 */
-	public PictureWallEffect(DatabaseManager dm){
+	public PictureWallEffect(DatabaseManager dm) {
 		super(dm);
 	}
-	
-	public void setup(String pathname){
+
+	public void setup(String pathname) {
 		this.pathname = pathname;
 	}
 
@@ -60,22 +60,24 @@ public class PictureWallEffect extends Effect {
 			ImageStorage imgStore = new ImageStorage();
 
 			List<String> photoids = new LinkedList<String>();
-			for (int i=0; i<individuals.size(); i++){
-				 List<AttributeReliabilityPair> pairs = individuals.get(i).getProperties().get("fb_photo");
-				 for (int j=0; j<pairs.size(); j++){
-					 photoids.add((String) pairs.get(i).getAttribute());
-				 }
+			for (int i = 0; i < individuals.size(); i++) {
+				List<AttributeReliabilityPair> pairs = individuals.get(i)
+						.getProperties().get("fb_photo");
+				for (int j = 0; j < pairs.size(); j++) {
+					photoids.add((String) pairs.get(i).getAttribute());
+				}
 			}
-			
+
 			BufferedImage[] photos = new BufferedImage[photoids.size()];
 			for (int i = 0; i < photos.length; i++) {
-				photos[i] = (BufferedImage) imgStore.retreiveImage(photoids.get(i));
+				photos[i] = (BufferedImage) imgStore.retreiveImage(photoids
+						.get(i));
 			}
 
 			BufferedImage image;
 			if (photos.length > 5) {
 				// assemble into columns of 5
-				int numcols = photos.length / 5; //individuals.size() / 5;
+				int numcols = photos.length / 5; // individuals.size() / 5;
 				int[] colwidth = new int[numcols];
 				BufferedImage[] colPhotos = new BufferedImage[numcols];
 				for (int i = 0; i < numcols; i++) {
@@ -90,13 +92,13 @@ public class PictureWallEffect extends Effect {
 					int heightCurr = 0;
 					BufferedImage colImage = new BufferedImage(colwidth[i],
 							heightTotal, BufferedImage.TYPE_INT_RGB);
-					Graphics2D g2d = colImage.createGraphics();
 					for (int j = 0; j < 5; j++) {
+						Graphics2D g2d = colImage.createGraphics();
 						g2d.drawImage(photos[i + j], 0, heightCurr, null);
 						heightCurr += photos[i + j].getHeight();
+						g2d.dispose();
 					}
 					colPhotos[i] = colImage;
-					g2d.dispose();
 				}
 
 				// assemble columns into 1 image
@@ -105,10 +107,10 @@ public class PictureWallEffect extends Effect {
 				image = concatenateByWidth(photos);
 			}
 
-			//ummm pathname?? need input?
+			// ummm pathname?? need input?
 			ImageIO.write(image, "png", new File("pictureEffect.png"));
 		} catch (InvalidReliabilityException e) {
-			//TODO 
+			// TODO
 			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -127,10 +129,9 @@ public class PictureWallEffect extends Effect {
 			e.printStackTrace();
 		}
 
-
 	}
-	
-	private BufferedImage concatenateByWidth(BufferedImage[] photos){
+
+	private BufferedImage concatenateByWidth(BufferedImage[] photos) {
 		int widthTotal = 0;
 		int height = 0;
 		for (int i = 0; i < photos.length; i++) {
@@ -144,16 +145,16 @@ public class PictureWallEffect extends Effect {
 		System.out.println("w: " + widthTotal + "  h: " + height);
 		BufferedImage image = new BufferedImage(widthTotal, height,
 				BufferedImage.TYPE_INT_RGB);
-		Graphics2D g2d = image.createGraphics();
 		for (int i = 0; i < photos.length; i++) {
+			System.out.println(i);
+			Graphics2D g2d = image.createGraphics();
 			g2d.drawImage(photos[i], widthCurr, 0, null);
 			widthCurr += photos[i].getWidth();
-		
-		g2d.dispose();
-	}
+			g2d.dispose();
+		}
 		return image;
 	}
-	
+
 	/**
 	 * Method solely for testing the PicuteWallEffect
 	 * 
@@ -164,13 +165,50 @@ public class PictureWallEffect extends Effect {
 	 * @throws SQLException
 	 * @throws InvalidAttributeNameException
 	 */
-	public static void main(String[] args) throws ClassNotFoundException, ConfigFileNotFoundException, IOException, SQLException, InvalidAttributeNameException{
-		Hashtable<String,AttributeCategories> globalAttributeTable = new Hashtable<String,AttributeCategories>();
+	public static void main(String[] args) throws ClassNotFoundException,
+			ConfigFileNotFoundException, IOException, SQLException,
+			InvalidAttributeNameException {
+		Hashtable<String, AttributeCategories> globalAttributeTable = new Hashtable<String, AttributeCategories>();
 		globalAttributeTable.put("fb_photo", AttributeCategories.PHOTO);
-		SecondaryDatabaseManager dm = new SecondaryDatabaseManager(globalAttributeTable);
+		SecondaryDatabaseManager dm = new SecondaryDatabaseManager(
+				globalAttributeTable);
 		PictureWallEffect pwe = new PictureWallEffect(dm);
 		Show show = dm.getAllShows().get(2);
+
 		pwe.execute(show);
+
+		/*BufferedImage[] photos = new BufferedImage[6];
+		File img = new File("storage/10A_0008.JPG");
+		BufferedImage buffImg = new BufferedImage(1536, 1024,
+				BufferedImage.TYPE_INT_ARGB);
+		buffImg = ImageIO.read(img);
+		photos[0] = buffImg;
+		img = new File("storage/11A_0009.JPG");
+		buffImg = new BufferedImage(1536, 1024, BufferedImage.TYPE_INT_ARGB);
+		buffImg = ImageIO.read(img);
+		photos[1] = buffImg;
+		img = new File("storage/12A_0010.JPG");
+		buffImg = new BufferedImage(1536, 1024, BufferedImage.TYPE_INT_ARGB);
+		buffImg = ImageIO.read(img);
+		photos[2] = buffImg;
+		img = new File("storage/13A_0011.JPG");
+		buffImg = new BufferedImage(1536, 1024, BufferedImage.TYPE_INT_ARGB);
+		buffImg = ImageIO.read(img);
+		photos[3] = buffImg;
+		img = new File("storage/14A_0012.JPG");
+		buffImg = new BufferedImage(1536, 1024, BufferedImage.TYPE_INT_ARGB);
+		buffImg = ImageIO.read(img);
+		photos[4] = buffImg;
+		img = new File("storage/15A_0013.JPG");
+		buffImg = new BufferedImage(1536, 1024, BufferedImage.TYPE_INT_ARGB);
+		buffImg = ImageIO.read(img);
+		photos[5] = buffImg;
+
+		buffImg = pwe.concatenateByWidth(photos);
+
+		ImageIO.write(buffImg, "png", new File("storage/pictureEffectTest.png"));
+		*/
+
 	}
-	
+
 }
